f2e1da70ccaa9bd3a3404d14d2d5cb2cc3b5e42d
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2018-05-01 20:17:08 -0700

    modified list

--- vowels.py
+++ vowels.py
@@ -1,6 +1,9 @@
 vowels = ['a', 'e', 'i','o','u']
-word = "Milliways"
+word = input('Provide a word to search for vowels:')
+found = []
 for letter in word:
 	if letter in vowels:
 		print(letter)
-
+		found.append(letter)
+for vowel in found:
+        print(vowel)
