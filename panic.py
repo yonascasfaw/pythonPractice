e7bcea930909f1d8c1a6d386a7e46a96c1c2c0aa
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2018-05-03 00:53:28 -0700

    added panic.py

---
+++ panic.py
@@ -0,0 +1,4 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
--- vowels.py
+++ vowels.py
@@ -2,8 +2,8 @@ vowels = ['a', 'e', 'i','o','u']
 word = input('Provide a word to search for vowels:')
 found = []
 for letter in word:
-	if letter in vowels:
-		print(letter)
-		found.append(letter)
+        if letter in vowels:
+                if letter not in found:
+                        found.append(letter)
 for vowel in found:
         print(vowel)
