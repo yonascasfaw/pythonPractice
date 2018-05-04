274f4ede8a03bb5c8c3363d0e2fd776614851818
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2018-05-04 00:04:47 -0700

    finished list methods

--- panic.py
+++ panic.py
@@ -2,3 +2,15 @@ phrase = "Don't panic!"
 plist = list(phrase)
 print(phrase)
 print(plist)
+
+#new_phrase = plist[1:8]
+#new_phrase.remove("'")
+#new_phrase.extend([new_phrase.pop(),new_phrase.pop()])
+#new_phrase.insert(2,new_phrase.pop(3))
+
+new_phrase =''.join(plist[1:3])
+
+new_phrase = new_phrase + ''.join([plist[5],plist[4],plist[7],plist[6]])
+
+#new_phrase = ''.join(new_phrase)   
+print(new_phrase)
