2787affaeea14ad01a58da0fc90597bae9851d65
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2018-05-06 17:25:08 -0700

    not in frequency count

--- frequencyCountMagnets.py
+++ frequencyCountMagnets.py
@@ -9,7 +9,8 @@ found = {}
 
 for letter in word:
     if letter in vowels:
-        found.setdefault(letter, 0)
+        if letter not in found:
+            found[letter] = 1
         found[letter] += 1
     
 for k, v in sorted(found.items()):
