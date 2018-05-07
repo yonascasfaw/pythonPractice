61735d3d408226d1f85115348608d9c5d2828255
yonascasfaw@gmail.com <yonascasfaw@gmail.com>
2018-05-06 18:06:27 -0700

    set for vowel count

--- frequencyCountMagnets.py
+++ frequencyCountMagnets.py
@@ -1,19 +1,8 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
+vowels = {'a', 'e', 'i', 'o', 'u'}
 word = input("Provide a word to search for vowels: ")
-found = {}
-##found['a'] = 0
-##found['e'] = 0
-##found['i'] = 0
-##found['o'] = 0
-##found['u'] = 0
-
-for letter in word:
-    if letter in vowels:
-        if letter not in found:
-            found[letter] = 1
-        found[letter] += 1
+found = vowels.intersection(set(word))
     
-for k, v in sorted(found.items()):
-    print(k, 'was found', v, 'time(s).')
+for vowel in found:
+    print(vowel)
 
     
